84ccb53222bce6a081c480557a8f182c4d1b5fe4
jacky nguyen <nguyenhoang96.arsenal@gmail.com>
2023-04-12 09:09:00 +0700

    update index

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,31 +1,11 @@
 import '@/styles/globals.scss';
 import type { AppProps } from 'next/app';
-import Head from 'next/head';
 import Image from 'next/image';
 import Link from 'next/link';
-import GoogleAnalytics from '../components/GoogleAnalytics';
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <>
-      <div>
-        <Head>
-          <title>Hoang&apos;s Blog</title>
-          <meta name="description" content="Một chiếc blog nho nhỏ" />
-          <meta property="og:image" content="/favicon.png" />
-          <meta property="og:title" content="Hoang's Blog" />
-          <meta property="og:description" content="Một chiếc blog nho nhỏ" />
-          <meta name="keywords" content="nguyenhoang, blog, it"></meta>
-          <meta name="author" content="Hoang Nguyen"></meta>
-          <meta name="google-site-verification" content="2sv7utYTeZ0DN0LfK10EIjz-paQ_f4mP84RWLmQqjs0" />
-          <link rel="icon" href="/favicon.png" />
-        </Head>
-      </div>
-      <div>
-        {process.env.NEXT_PUBLIC_G_ID ? (
-          <GoogleAnalytics gaId={process.env.NEXT_PUBLIC_G_ID} />
-        ) : null}
-      </div>
       <div className="container">
         <div className="logo-container">
           <Link href="/" className="logo pointer">
